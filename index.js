fe719ace9bee9dd071ae051181f91a3ac40430b0
tga55a <tgardner2@my.waketech.edu>
2025-02-14 11:16:53 -0500

    Add files via upload

---
+++ index.js
@@ -0,0 +1,30 @@
+
+let index = 1;
+let active = true
+
+while (active) {
+
+    let randomNumber = Math.floor(Math.random() * 10) + 1
+
+    while (active) {
+        try {
+            let guess = Number(prompt("Question #" + index + ". What's your guess?: "))
+            console.log(guess)
+            let attempts = 0
+            
+            if (guess == randomNumber) {
+                window.prompt("Correct! It took you " + attempts + " attempts. Press ENTER to continue")
+                break
+            } else if (guess == 999) {
+                active = false
+            } else {
+                window.prompt("Incorrect, guess again. Press ENTER to try again.")
+                attempts += 1
+            }
+
+        } catch {
+            window.prompt("I cannot understand this input. Please enter a number.")
+        }
+    }
+    index += 1
+}
