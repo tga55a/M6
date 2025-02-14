2d49355417698d7fc3edda582786fc085918fd80
tga55a <tgardner2@my.waketech.edu>
2025-02-14 11:44:17 -0500

    Update index.js

--- index.js
+++ index.js
@@ -3,14 +3,11 @@ let index = 1;
 let active = true
 
 while (active) {
-
     let randomNumber = Math.floor(Math.random() * 10) + 1
     let attempts = 1
-
     while (active) {
         try {
             let guess = Number(prompt("Question #" + index + ". What's your guess?: "))
-            
             if (guess == randomNumber) {
                 window.prompt("Correct! It took you " + attempts + " attempts. Press ENTER to continue")
                 break
@@ -20,7 +17,6 @@ while (active) {
                 window.prompt("Incorrect, guess again. Press ENTER to try again.")
                 attempts += 1
             }
-
         } catch {
             window.prompt("I cannot understand this input. Please enter a number next time. Press ENTER to try again.")
         }
